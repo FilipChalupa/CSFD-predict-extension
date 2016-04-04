394087b812a6eb8557d9a59925a43c0030925091
Filip Chalupa <chalupa.filip@gmail.com>
2016-04-04 23:14:18 +0200

    Show result

--- content.js
+++ content.js
@@ -54,9 +54,28 @@
 			total.weight += ratings[key]
 		}
 		if (total.weight > 0) {
-			total.rating = total.rating / total.weight
+			return total.rating / total.weight
 		}
-		return total.rating
+		return false
+	}
+
+	function showPrediction(rating) {
+		var wrapper = document.querySelector('#rating .average')
+		if (wrapper) {
+			var prediction = document.createElement('span')
+			prediction.innerHTML = Math.round(rating) + '%'
+			prediction.title = 'Vaše předpokládané hodnocení'
+			prediction.style.fontSize = '40%'
+			prediction.style.position = 'absolute'
+			prediction.style.top = '0'
+			prediction.style.right = '0'
+			prediction.style.padding = '5px 10px'
+			prediction.style.background  = '#059'
+			prediction.style.borderTopRightRadius = '10px'
+			prediction.style.borderBottomLeftRadius = '10px'
+			prediction.style.cursor = 'help'
+			wrapper.appendChild(prediction)
+		}
 	}
 
 
@@ -78,7 +97,10 @@
 				chrome.storage.local.set({movies: movies})
 			} else {
 				// Predict rating
-				console.log('I predict '+getPrediction(text, movies)+' %')
+				var prediction = getPrediction(text, movies)
+				if (prediction !== false) {
+					showPrediction(prediction)
+				}
 			}
 
 		})
